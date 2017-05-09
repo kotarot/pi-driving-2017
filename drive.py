4cef9a467db98a7fd27b847368793a9e584dc49e
Kotaro Terada <tera@toki.waseda.jp>
2017-05-09 14:49:44 +0900

    Add 2017-05-09 work

--- drive.py
+++ drive.py
@@ -1,4 +1,9 @@
 import pygame
+import subprocess
+import datetime
+import os
+
+basepath = os.path.abspath(os.path.dirname(__file__))
 
 pygame.init()
 
@@ -10,14 +15,22 @@ while  True:
     for event in pygame.event.get():
         if event.type == pygame.JOYBUTTONDOWN:
             print(event.__dict__)
+            todaydetail = datetime.datetime.today()
+            cmd = ''
+            if event.__dict__['button'] == 0:
+                cmd = "raspistill -o '" + basepath + "/images/image_" + str(todaydetail) + ".jpg' -t 1 -n"
+            elif event.__dict__['button'] == 1:
+                cmd = "raspistill -o '" + basepath + "/images/image_" + str(todaydetail) + ".jpg' -t 1 -n -ifx gpen"
+            if cmd != '':
+                subprocess.call(cmd,shell=True)
         if event.type == pygame.JOYBUTTONUP:
             print(event.__dict__)
 
     hat = joystick.get_hat(0)
-    print("Hat {} value: {}".format(0, str(hat)))
+    #print("Hat {} value: {}".format(0, str(hat)))
 
     axes = joystick.get_numaxes()
     for i in range(axes):
         axis = joystick.get_axis(i)
-        print("Axis {} value: {}".format(i, axis))
+        #print("Axis {} value: {}".format(i, axis))
 
